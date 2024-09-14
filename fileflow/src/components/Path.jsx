d5ba62b9d7b12238920cf16dd66769b483de172c
David Martinez Gil <davidmartinezgilp@gmail.com>
2024-09-13 22:40:35 -0400

    deleted table component added Path

---
+++ fileflow/src/components/Path.jsx
@@ -0,0 +1,12 @@
+function Path({name, size}){
+    return(
+        <div>
+            <p>Name</p>
+            <p>{name}</p>
+            <p>Size</p>
+            <p>{size}</p>
+        </div>
+    );
+}
+
+export default Path;
