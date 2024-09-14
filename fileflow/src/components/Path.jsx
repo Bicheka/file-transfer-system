4c1e374714580790a9b8c1e498496dc6a3498a7e
David Martinez Gil <davidmartinezgilp@gmail.com>
2024-09-14 19:40:36 -0400

    added styles to path component

--- fileflow/src/components/Path.jsx
+++ fileflow/src/components/Path.jsx
@@ -1,10 +1,36 @@
-function Path({name, size}){
-    return(
-        <div>
-            <p>Name</p>
-            <p>{name}</p>
-            <p>Size</p>
-            <p>{size}</p>
+function Path({ data }) {
+    return (
+        <div className="bg-gray-100 rounded-lg shadow-md flex flex-wrap items-center space-x-4 w-full max-w-4xl mx-auto">
+            <div className="flex items-center space-x-1 px-4">
+                <p className="font-semibold text-gray-700 text-xs md:text-sm">Name:</p>
+                <p className="text-gray-900 text-xs md:text-sm">{data.name}</p>
+            </div>
+
+            <div className="flex items-center space-x-1">
+                <p className="font-semibold text-gray-700 text-xs md:text-sm">Size:</p>
+                <p className="text-gray-900 text-xs md:text-sm">{data.size}</p>
+            </div>
+
+            <div className="flex items-center space-x-1">
+                <p className="font-semibold text-gray-700 text-xs md:text-sm">Path:</p>
+                <p className="text-gray-900 truncate text-xs md:text-sm">{data.path}</p>
+            </div>
+            <div className="block w-full">
+                <p className="text-gray-700 text-xs md:text-sm">Progress: {data.progress}%</p>
+            </div>
+            <div className="flex items-center space-x-1">
+                <p className="font-semibold text-gray-700 text-xs md:text-sm">Uploading to:</p>
+                <p className="text-gray-900 text-xs md:text-sm">{data.uploadDestination}</p>
+            </div>
+
+            <div className="flex space-x-2 mt-2 md:mt-0">
+                <button className="bg-red-500 text-white px-2 py-1 rounded-lg text-xs hover:bg-red-600">
+                    Stop
+                </button>
+                <button className="bg-red-500 text-white px-2 py-1 rounded-lg text-xs hover:bg-red-600">
+                    Delete
+                </button>
+            </div>
         </div>
     );
 }
--- fileflow/src/components/Uploads.jsx
+++ fileflow/src/components/Uploads.jsx
@@ -1,7 +1,17 @@
+import Path from "./Path";
+
+const data = {
+    name: "elden ring", 
+    size: "40 GB", 
+    path: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+    progress: "--------------------------------------"
+};
+
 function Uploads(){
     return(
         <div>
             <h1>Hello Uploads</h1>
+            <Path data={data}/>
         </div>
     );
 }
