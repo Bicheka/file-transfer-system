0e4d4d2b70298faeb78008900fbb0043269dbd8b
David Martinez Gil <davidmartinezgilp@gmail.com>
2024-09-15 02:49:29 -0400

    added conditional rendering to Path

--- fileflow/src/components/Path.jsx
+++ fileflow/src/components/Path.jsx
@@ -1,4 +1,4 @@
-function Path({ data }) {
+function Path({ data, updown }) {
     return (
         <div className="bg-gray-100 rounded-lg shadow-md flex flex-wrap items-center space-x-4 w-full max-w-4xl mx-auto">
             <div className="flex items-center space-x-1 px-4">
@@ -15,21 +15,51 @@ function Path({ data }) {
                 <p className="font-semibold text-gray-700 text-xs md:text-sm">Path:</p>
                 <p className="text-gray-900 truncate text-xs md:text-sm">{data.path}</p>
             </div>
-            <div className="block w-full">
-                <p className="text-gray-700 text-xs md:text-sm">Progress: {data.progress}%</p>
-            </div>
-            <div className="flex items-center space-x-1">
-                <p className="font-semibold text-gray-700 text-xs md:text-sm">Uploading to:</p>
-                <p className="text-gray-900 text-xs md:text-sm">{data.uploadDestination}</p>
-            </div>
+            {
+                data.isTransfering && 
+                <div className="flex-wrap sm:flex items-center w-full">    
+                    
+                    {   
+                        updown == "upload" ?
+                        <p className="text-green-500 font-medium py-1 rounded-lg text-xs w-fit sm:mr-2">
+                            Uploading
+                        </p>
+                        :
+                        <p className="text-green-500 font-medium py-1 rounded-lg text-xs w-fit sm:mr-2">
+                            Downloading
+                        </p>
+                    }
+                    
+                    <p className="text-gray-700 text-xs md:text-sm w-fit">Progress: {data.progress}%</p>
+                </div>
+                }
+            {   
+                data.isTransfering &&
+                <div className="flex items-center space-x-1">
+                    <p className="font-semibold text-gray-700 text-xs md:text-sm">Uploading to:</p>
+                    <p className="text-gray-900 text-xs md:text-sm">{data.uploadDestination}</p>
+                </div>
+            }
 
             <div className="flex space-x-2 mt-2 md:mt-0">
-                <button className="bg-red-500 text-white px-2 py-1 rounded-lg text-xs hover:bg-red-600">
-                    Stop
-                </button>
-                <button className="bg-red-500 text-white px-2 py-1 rounded-lg text-xs hover:bg-red-600">
-                    Delete
-                </button>
+                {
+                    data.isTransfering && 
+                    <button className="bg-red-500 text-white px-2 py-1 rounded-lg text-xs hover:bg-red-600">
+                        Stop
+                    </button>
+                }
+                {
+                    updown == "upload" &&
+                    <button className="bg-red-500 text-white px-2 py-1 rounded-lg text-xs hover:bg-red-600">
+                        Delete
+                    </button>
+                }
+                {
+                    updown == "download" && !data.isTransfering &&
+                    <button className="bg-green-500 text-white px-2 py-1 rounded-lg text-xs hover:bg-red-600">
+                        Download
+                    </button>
+                }
             </div>
         </div>
     );
--- fileflow/src/components/Uploads.jsx
+++ fileflow/src/components/Uploads.jsx
@@ -4,14 +4,15 @@ const data = {
     name: "elden ring", 
     size: "40 GB", 
     path: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
-    progress: "--------------------------------------"
+    progress: "--------------------------------------",
+    isTransfering: true
 };
 
 function Uploads(){
     return(
         <div>
             <h1>Hello Uploads</h1>
-            <Path data={data}/>
+            <Path data={data} updown="download"/>
         </div>
     );
 }
