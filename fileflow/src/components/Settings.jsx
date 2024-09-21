1d2e0e77ecc4c262c7274164eaf4cc61ec079e25
David Martinez Gil <davidmartinezgilp@gmail.com>
2024-09-21 00:04:17 -0400

    added color theme selection html
    added html form to select color theme on settings although still not functional

--- fileflow/src/components/Settings.jsx
+++ fileflow/src/components/Settings.jsx
@@ -8,6 +8,14 @@ function Settings() {
                 <label>Start server on launch</label>
                 <input className="mx-5" type="checkbox"/>
             </div>
+            <div>
+                <label>Color theme</label>
+                <label htmlFor="cars">theme:</label>
+                <select name="cars" id="cars">
+                    <option value="volvo">light</option>
+                    <option value="saab">Dark</option>
+                </select>
+            </div>
         </div>
     );
 }
