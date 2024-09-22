41553cf0200f25f8fb33151fe8fbc3f098fa9ddf
David Martinez Gil <davidmartinezgilp@gmail.com>
2024-09-22 02:27:02 -0400

    added conditional button to connect to one of the ips from the list

--- fileflow/src/components/Known.jsx
+++ fileflow/src/components/Known.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Known(){
     // TODO create function to get list of known ips from server
     const knownIps = [
@@ -66,7 +68,11 @@ function Known(){
         {name: "david", ip: "192.0.2.1", ipType: "ipv4"},
         {name: "mario", ip: "2001:db8:0:1234:0:567:8:1", ipType: "ipv6"},
     ]
+    const [clickedRowIndex, setClickedRowIndex] = useState(null);
 
+    const handleClick = (index) => {
+        setClickedRowIndex(index === clickedRowIndex ? null : index);
+    };
     return (
     <div className="h-full overflow-y-auto min-w-fit">
         <table className="min-w-full table-auto">
@@ -79,11 +85,22 @@ function Known(){
             </thead>
             <tbody>
                 {knownIps.map((entry, index) => (
-                    <tr key={index}>
-                        <td className="px-4 py-2">{entry.name}</td>
-                        <td className="px-4 py-2">{entry.ip}</td>
-                        <td className="px-4 py-2">{entry.ipType}</td>
-                    </tr>
+                    <>
+                        <tr className="cursor-pointer hover:bg-gray-100" key={index} onClick={() => handleClick(index)}>
+                            <td className="px-4 py-2">{entry.name}</td>
+                            <td className="px-4 py-2">{entry.ip}</td>
+                            <td className="px-4 py-2">{entry.ipType}</td>
+                        </tr>
+                        {clickedRowIndex === index && (
+                            <tr>
+                                <td colSpan="3" className="px-4 py-2 text-center">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded">
+                                        Connect to {entry.name}
+                                    </button>
+                                </td>
+                            </tr>
+                        )}
+                    </>
                 ))}
             </tbody>
         </table>
