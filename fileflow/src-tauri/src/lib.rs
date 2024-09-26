8f0d2a0fe77d5ac84f83bacdcf00469343c7be62
David Martinez Gil <davidmartinezgilp@gmail.com>
2024-09-25 20:02:02 -0400

    added dialog to select file/directory path

--- fileflow/src-tauri/src/lib.rs
+++ fileflow/src-tauri/src/lib.rs
@@ -45,6 +45,7 @@ pub fn run() {
         Ok(())
     })
     .plugin(tauri_plugin_shell::init())
+    .plugin(tauri_plugin_dialog::init())
     .run(tauri::generate_context!())
     .expect("error while running the application");
 }
--- fileflow/src/components/List.jsx
+++ fileflow/src/components/List.jsx
@@ -1,8 +1,10 @@
+import FilePicker from "./FilePicker";
 import Item from "./Item";
 
 function List({ list }) {
   return (
-    <div className="m-auto h-full w-[%100] overflow-y-auto rounded-lg border-2 border-gray-200 pb-16">
+    <div className="m-auto h-full w-[%100] overflow-y-auto rounded-lg border-2 border-gray-200 bg-slate-100 pb-16 pt-10">
+      <FilePicker/>
       {list.map((i) => (
         // eslint-disable-next-line react/jsx-key
         <Item data={i} updown="upload" />
