16eacc8fe6eb4dfa42ed9d0524ee92240e21743f
David Martinez Gil <davidmartinezgilp@gmail.com>
2024-10-06 23:58:10 -0400

    conditionally allow folder picker in dialog based on os

--- fileflow/src/components/FilePicker.jsx
+++ fileflow/src/components/FilePicker.jsx
@@ -1,4 +1,5 @@
 import { open } from "@tauri-apps/plugin-dialog";
+import { platform } from "@tauri-apps/plugin-os";
 import { useState } from "react";
 const FilePicker = () => {
   const [selectedPath, setSelectedPath] = useState("");
@@ -6,8 +7,13 @@ const FilePicker = () => {
   const selectFileOrFolder = async () => {
     try {
       // Open the file/folder picker dialog
+      const currentPlatform = await platform();
+      let directoryAllowed = true;
+      if (currentPlatform === 'ios' | currentPlatform === 'android'){
+        directoryAllowed = false
+      }
       const path = await open({
-        directory: true, // Set to `true` if you want to select a folder
+        directory: directoryAllowed, // Set to `true` if you want to select a folder
         multiple: false, // Set to `true` if you want to select multiple files
       });
 
