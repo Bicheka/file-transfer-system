fbb700bc77cce705c4bfd872b0f77281749f2abe
David Martinez Gil <davidmartinezgilp@gmail.com>
2024-10-12 22:21:58 -0400

    defined p2p logic, server, and network into features

--- fts/src/lib.rs
+++ fts/src/lib.rs
@@ -1,11 +1,13 @@
 
 
-// #[cfg(feature = "server")]
+#[cfg(feature = "server")]
 pub mod server;
 pub mod client;
+#[cfg(feature = "p2p")]
 pub mod p2p;
 #[cfg(windows)]
 pub mod graceful_shutdown;
 
+#[cfg(feature = "p2p")]
 pub mod network;
 pub mod file_transfer;
