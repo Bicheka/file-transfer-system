447323903765225119cc543fbe3ffb595acf8d7c
David Martinez Gil <davidmartinezgilp@gmail.com>
2024-11-11 16:10:17 -0500

    improve zipping logic to increase speed

--- src/compression.rs
+++ src/compression.rs
@@ -1,56 +1,77 @@
 use anyhow::Context;
+use zip::ZipArchive;
 use std::io::prelude::*;
-use zip::{result::ZipError, write::SimpleFileOptions, ZipArchive};
-
+use std::sync::{Arc, Mutex};
+use zip::{result::ZipError, write::SimpleFileOptions, ZipWriter};
 use std::fs::File;
 use std::path::Path;
 use walkdir::{DirEntry, WalkDir};
 
-
 pub fn zip_dir<T>(
     it: &mut dyn Iterator<Item = DirEntry>,
     prefix: &Path,
-    writer: T,
+    writer: Arc<Mutex<ZipWriter<T>>>,
     method: zip::CompressionMethod,
 ) -> anyhow::Result<()>
 where
     T: Write + Seek,
 {
-    let mut zip = zip::ZipWriter::new(writer);
     let options = SimpleFileOptions::default()
         .compression_method(method)
         .unix_permissions(0o755);
 
     let prefix = Path::new(prefix);
     let mut buffer = Vec::new();
-    for entry in it {
+
+    // Process each entry in parallel
+    it.for_each(|entry| {
         let path = entry.path();
         let name = path.strip_prefix(prefix).unwrap();
         let path_as_string = name
             .to_str()
             .map(str::to_owned)
-            .with_context(|| format!("{name:?} Is a Non UTF-8 Path"))?;
+            .with_context(|| format!("{name:?} is a Non UTF-8 Path"))
+            .unwrap();
 
-        // Write file or directory explicitly
-        // Some unzip tools unzip files with directory paths correctly, some do not!
         if path.is_file() {
-            println!("adding file {path:?} as {name:?} ...");
-            zip.start_file(path_as_string, options)?;
-            let mut f = File::open(path)?;
+            println!("Adding file {path:?} as {name:?}...");
+            let mut f = File::open(path).unwrap();
+            f.read_to_end(&mut buffer).unwrap();
 
-            f.read_to_end(&mut buffer)?;
-            zip.write_all(&buffer)?;
+            // Lock writer to safely access ZipWriter
+            let mut zip = writer.lock().unwrap();
+            zip.start_file(path_as_string, options).unwrap();
+            zip.write_all(&buffer).unwrap();
             buffer.clear();
         } else if !name.as_os_str().is_empty() {
-            // Only if not root! Avoids path spec / warning
-            // and mapname conversion failed error on unzip
-            println!("adding dir {path_as_string:?} as {name:?} ...");
-            zip.add_directory(path_as_string, options)?;
+            println!("Adding dir {path_as_string:?} as {name:?}...");
+            let mut zip = writer.lock().unwrap();
+            zip.add_directory(path_as_string, options).unwrap();
         }
-    }
-    zip.finish()?;
+    });
+
     Ok(())
 }
+
+pub fn start_compressing(src_dir: &Path, dst_file: &Path, method: zip::CompressionMethod) -> anyhow::Result<()> {
+    if !src_dir.is_dir() {
+        return Err(ZipError::FileNotFound.into());
+    }
+
+    let path = Path::new(dst_file);
+    let file = File::create(path).unwrap();
+    let zip_writer = ZipWriter::new(file);
+    let writer = Arc::new(Mutex::new(zip_writer));
+
+    let walkdir = WalkDir::new(src_dir);
+    let it = walkdir.into_iter();
+
+    zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, writer, method)?;
+
+    Ok(())
+}
+
+
 
 pub fn unzip_file(zip_path: &str, output_dir: &str) -> zip::result::ZipResult<()> {
     // Open the ZIP file
@@ -85,21 +106,5 @@ pub fn unzip_file(zip_path: &str, output_dir: &str) -> zip::result::ZipResult<()
             }
         }
     }
-    Ok(())
-}
-
-pub fn start_compressing(src_dir: &Path, dst_file: &Path, method: zip::CompressionMethod) -> anyhow::Result<()> {
-    if !Path::new(src_dir).is_dir() {
-        return Err(ZipError::FileNotFound.into());
-    }
-
-    let path = Path::new(dst_file);
-    let file = File::create(path).unwrap();
-
-    let walkdir = WalkDir::new(src_dir);
-    let it = walkdir.into_iter();
-
-    zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
-
     Ok(())
 }
